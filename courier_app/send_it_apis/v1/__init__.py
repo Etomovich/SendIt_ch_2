e3e817ec9dbf719263700c00e2c1b3e545d32072
Etomovich <etolejames@gmail.com>
2018-11-11 11:26:57 +0300

    [Chore #161864131]Created parcel model views for better interaction with parcels model class.

--- courier_app/send_it_apis/v1/__init__.py
+++ courier_app/send_it_apis/v1/__init__.py
@@ -4,6 +4,10 @@ from flask_restful import Resource, Api
 from courier_app.send_it_apis.v1.views.user_views import CreateUser,\
     UserLogin, FetchAllUsers, TheUser
 
+from courier_app.send_it_apis.v1.views.parcel_views import AllParcels,\
+    AParcels, UserParcels, CancelParcels, ApprovedParcels,\
+    NotStartedParcels,InTransitParcels, CancelledParcels, DeliveredParcels
+
 bp = Blueprint("my_api_v1", __name__)
 
 v1_bp = Api(bp)
@@ -13,6 +17,17 @@ v1_bp.add_resource(CreateUser,"/register")
 v1_bp.add_resource(UserLogin,"/login")
 v1_bp.add_resource(FetchAllUsers,"/users")
 v1_bp.add_resource(TheUser,"/user/<int:user_id>")
+
+#Parcel routes
+v1_bp.add_resource(AllParcels,"/parcels")
+v1_bp.add_resource(AParcels,"/parcels/<int:parcel_id>")
+v1_bp.add_resource(UserParcels,"/users/<int:user_id>/parcels")
+v1_bp.add_resource(CancelParcels,"/parcels/<int:parcel_id>/cancel")
+v1_bp.add_resource(ApprovedParcels,"/parcels/<int:user_id>/approved")
+v1_bp.add_resource(NotStartedParcels,"/parcels/<int:user_id>/not-started")
+v1_bp.add_resource(InTransitParcels,"/parcels/<int:user_id>/in-transit")
+v1_bp.add_resource(CancelledParcels,"/parcels/<int:user_id>/cancelled")
+v1_bp.add_resource(DeliveredParcels,"/parcels/<int:user_id>/delivered")
 
 
 from courier_app.send_it_apis.v1.models.send_it_users import SystemUsers
